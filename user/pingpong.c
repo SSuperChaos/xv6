7ad8ec0a802d57c482a9084c3f1080e10391ae2e
camellia2027 <2653614124@qq.com>
2024-03-15 20:49:35 +0800

    格式化所有代码

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,20 +1,17 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     char buf;
     // pipefd[0] read end
     // pipefd[1] write end
     int pipefd[2];
-    if (pipe(pipefd) == -1)
-    {
+    if (pipe(pipefd) == -1) {
         fprintf(2, "Cannot create a pipe\n");
         exit(-1);
     }
     int child_pid = fork();
-    if (child_pid == -1)
-    {
+    if (child_pid == -1) {
         fprintf(2, "Cannot fork a child process\n");
         exit(-1);
     }
@@ -22,8 +19,7 @@ int main(int argc, char **argv)
     // parent process
     // 1st: send a byte to child
     // 2nd: recv a byte from child
-    if (child_pid > 0)
-    {
+    if (child_pid > 0) {
         char p_send = 'a';
         write(pipefd[1], &p_send, sizeof(p_send));
         close(pipefd[1]);
@@ -38,8 +34,7 @@ int main(int argc, char **argv)
     // child process
     // 1st: recv a byte from parent
     // 2nd: send a byte to parent
-    if (child_pid == 0)
-    {
+    if (child_pid == 0) {
         read(pipefd[0], &buf, sizeof(buf));
         printf("%d: received ping\n", getpid());
 
--- user/sleep.c
+++ user/sleep.c
@@ -3,15 +3,14 @@
 #include "user/user.h"
 
 int
-main(int argc, char **argv)
-{
-  int i;
+main(int argc, char** argv) {
+    int i;
 
-  if(argc < 2){
-    fprintf(2, "usage: sleep...\n");
-    exit(1);
-  }
-  for(i=1; i<argc; i++)
-    sleep(atoi(argv[i]));
-  exit(0);
+    if (argc < 2) {
+        fprintf(2, "usage: sleep...\n");
+        exit(1);
+    }
+    for (i = 1; i < argc; i++)
+        sleep(atoi(argv[i]));
+    exit(0);
 }
